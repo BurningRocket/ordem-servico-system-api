74170507b8aef19f6b836cf8ab2ef7d7f1f851b1
Lucas Eleutério <leleuteiobueno@gmail.com>
2023-04-19 14:50:15 -0300

    correção token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -8,11 +8,11 @@ export class AuthMiddleware {
     //FIXME: Not receiving the token from authorization header
     let token = req.headers['Authorization'] || req.headers['authorization'];
     
-    token = token.replace('Bearer ', '');
-        
     if (!token) {
       return res.status(403).send("Um token é nescessário para validar"); 
     }
+
+    token = token.replace('Bearer ', '');
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET || 'secret');
